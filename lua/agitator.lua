cf3f7d425f5f104e82c79dcb5478a5aad1871a65
Emmanuel Touzery <etouzery@gmail.com>
2021-11-20 21:21:12 +0100

    oops

--- lua/agitator.lua
+++ lua/agitator.lua
@@ -1,4 +1,4 @@
-local open_file_branch = require(agitator.open_file_branch)
+local open_file_branch = require('agitator.open_file_branch')
 
 return {
     open_file_git_branch = open_file_branch.open_file_git_branch
