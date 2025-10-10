2ff77098804d0c85d30dc105c7a172581cce0645
Guillaume BOEHM <git@mail.gboehm.com>
2025-10-10 07:45:52 +0200

    time_machine: Use # in sha compare
    Also added a couple comments for documentation

--- lua/agitator/time_machine.lua
+++ lua/agitator/time_machine.lua
@@ -68,7 +68,8 @@ end
 
 local function git_time_machine_focus(sha)
     for index, entry in ipairs(vim.b.time_machine_entries) do
-        if string.sub(entry.sha, 1, string.len(sha)) == sha then
+        -- The param sha could be a short sha, so check against the start of the time machine full sha
+        if string.sub(entry.sha, 1, #sha) == sha then
             vim.b.time_machine_cur_idx = index
             git_time_machine_display()
             return true
@@ -151,6 +152,7 @@ local function handle_time_machine(lines, opts)
     vim.b.time_machine_entries = parse_time_machine(lines)
     vim.b.time_machine_cur_idx = 1
     if #vim.b.time_machine_entries >= 1 then
+        -- Focus the given commit sha if possible
         if opts ~= nil and opts.commit_sha then
             if git_time_machine_focus(opts.commit_sha) then
                 return
